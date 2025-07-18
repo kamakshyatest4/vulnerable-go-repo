269635973801cc1b9e7a24b66a04f48e303fc387
kamakshya-endor <kkar@endor.ai>
2025-07-18 12:31:08 +0530

    jwt-go and golang

--- main.go
+++ main.go
@@ -6,8 +6,9 @@ import (
 	"github.com/antchfx/xmlquery"
 	_ "github.com/dgrijalva/jwt-go"
 	_ "github.com/gogo/protobuf/proto"
+
 	// - "github.com/gogs/gogs"
-	// _ "github.com/hashicorp/golang-lru"
+	_ "github.com/hashicorp/golang-lru"
 	// _ "github.com/owncast/owncast/logging"
 )
 
